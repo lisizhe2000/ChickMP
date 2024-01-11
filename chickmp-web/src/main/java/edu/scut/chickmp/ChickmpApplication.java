6d53edbe582ddf08a38be062380673d2c03188ae
lisizhe2000 <lisizhe2000@163.com>
2024-01-11 16:06:48 +0800

    接入SpringBoot

---
+++ chickmp-web/src/main/java/edu/scut/chickmp/ChickmpApplication.java
@@ -0,0 +1,11 @@
+package edu.scut.chickmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickmpApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(ChickmpApplication.class, args);
+        }
+}
