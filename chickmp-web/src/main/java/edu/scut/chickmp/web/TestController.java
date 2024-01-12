dca76bc41f955005c02069b9d7c3bc75bd024d4d
lisizhe2000 <lisizhe2000@163.com>
2024-01-12 20:35:01 +0800

    接入MysQL, Spring Data JPA

--- chickmp-web/src/main/java/edu/scut/chickmp/web/TestController.java
+++ chickmp-web/src/main/java/edu/scut/chickmp/web/TestController.java
@@ -1,15 +1,34 @@
 package edu.scut.chickmp.web;
 
+import com.alibaba.fastjson.JSON;
+import edu.scut.chickmp.support.dao.MessageTemplateDao;
+import edu.scut.chickmp.support.domain.MessageTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 public class TestController {
+
+    @Autowired
+    private MessageTemplateDao messageTemplateDao;
+
     @RequestMapping("/test")
     private String test() {
         log.info("Received!");
         return "Received.";
     }
+
+    @RequestMapping("/database")
+    private String testDatabase() {
+        List<MessageTemplate> list = messageTemplateDao.findAllByIsDeletedEquals(0, PageRequest.of(0, 10));
+        return JSON.toJSONString(list);
+    }
+
 }
